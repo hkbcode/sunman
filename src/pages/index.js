ca4eb8b1be7a74b03916dd2208a2a7319245d0f1
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2021-04-26 16:45:03 +0530

    added tailwindcss :fire:

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+
+const IndexPage = () =>{
+	return(
+		<div>
+			<h1 className="text-xl text-red-400"> Welcome to Tailwindcss </h1>
+		</div>
+		)
+}
+
+
+
+export default IndexPage
